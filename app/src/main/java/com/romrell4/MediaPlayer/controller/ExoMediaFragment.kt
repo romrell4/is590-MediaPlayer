1f3a8bddea653e46440186af801d91e7cce21feb
Eric Romrell <eric_romrell@byu.edu>
2018-03-14 11:23:34 -0600

    Added speed manipulation

--- app/src/main/java/com/romrell4/MediaPlayer/controller/ExoMediaFragment.kt
+++ app/src/main/java/com/romrell4/MediaPlayer/controller/ExoMediaFragment.kt
@@ -4,15 +4,13 @@ import android.app.Fragment
 import android.net.Uri
 import android.os.Bundle
 import android.view.*
-import com.google.android.exoplayer2.DefaultLoadControl
-import com.google.android.exoplayer2.DefaultRenderersFactory
-import com.google.android.exoplayer2.ExoPlayerFactory
-import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.*
 import com.google.android.exoplayer2.source.ExtractorMediaSource
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
 import com.romrell4.MediaPlayer.R
+import com.romrell4.MediaPlayer.support.CachingProxy
 import com.romrell4.MediaPlayer.support.inflateV2
 
 /**
@@ -36,6 +34,7 @@ class ExoMediaFragment: Fragment() {
     private var exoPlayerView: SimpleExoPlayerView? = null
     private lateinit var exoPlayer: SimpleExoPlayer
     private var videoMode = true
+    private var normalSpeedMode = true
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater?.inflateV2(R.layout.fragment_exo_media, container)
@@ -58,16 +57,27 @@ class ExoMediaFragment: Fragment() {
 
     override fun onOptionsItemSelected(item: MenuItem?) = when(item?.itemId) {
         R.id.toggle_media_type -> {
-            item.title = if (videoMode) "Video" else "Audio"
+            item.title = getString(if (videoMode) R.string.video else R.string.audio)
             videoMode = !videoMode
             setupPlayer()
+            true
+        }
+        R.id.toggle_speed -> {
+            item.title = getString(if (normalSpeedMode) R.string.normalSpeed else R.string.fastSpeed)
+            normalSpeedMode = !normalSpeedMode
+            exoPlayer.playbackParameters = PlaybackParameters(if (normalSpeedMode) 1f else 2f, 1f)
             true
         }
         else -> super.onOptionsItemSelected(item)
     }
 
     private fun setupPlayer() {
-        val uri = Uri.parse(arguments.getString(if (videoMode) VIDEO_URL_EXTRA else AUDIO_URL_EXTRA))
+        val url = if (videoMode) {
+            CachingProxy.getInstance(activity).getProxyUrl(arguments.getString(VIDEO_URL_EXTRA))
+        } else {
+            arguments.getString(AUDIO_URL_EXTRA)
+        }
+        val uri = Uri.parse(url)
         val mediaSource = ExtractorMediaSource.Factory(DefaultHttpDataSourceFactory("romrell4")).createMediaSource(uri)
         exoPlayer.prepare(mediaSource)
     }
