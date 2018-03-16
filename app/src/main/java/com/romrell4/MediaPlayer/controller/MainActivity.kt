eb645ba2e76deceffb9ed5944321c83b22711ddb
Eric Romrell <eric_romrell@byu.edu>
2018-03-15 19:32:58 -0600

    Implemented cache clear

--- app/src/main/java/com/romrell4/MediaPlayer/controller/MainActivity.kt
+++ app/src/main/java/com/romrell4/MediaPlayer/controller/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.romrell4.MediaPlayer.controller
 
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
@@ -16,5 +17,37 @@ class MainActivity: AppCompatActivity() {
                 .add(R.id.frameLayout, ConferencesFragment.onNewInstance())
                 .addToBackStack(null)
                 .commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.cache, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.delete_cache -> {
+            try {
+                val videoCacheDir = cacheDir.listFiles().first { it.name == "video-cache" }
+                val cachedFiles = videoCacheDir.listFiles()
+                val toDelete = arrayListOf<Int>()
+                AlertDialog.Builder(this)
+                        .setMultiChoiceItems(cachedFiles.map { it.name }.toTypedArray(), null, { _, which, isChecked ->
+                            if (isChecked) {
+                                toDelete.add(which)
+                            } else {
+                                toDelete.remove(which)
+                            }
+                        })
+                        .setPositiveButton("Delete", { dialog, which ->
+                            toDelete.map { cachedFiles.get(it) }.forEach { it.delete() }
+                        })
+                        .setNegativeButton("Cancel", null)
+                        .show()
+            } catch (ignore: NoSuchElementException) {
+            }
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 }
